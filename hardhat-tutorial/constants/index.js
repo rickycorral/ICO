b2d94e5e1ba27ca9b249b5ba08936524cf3b8b5e
rickycorral <rickycorral@hotmail.com>
2022-12-12 13:13:23 -0600

    firstt

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+// new ICO address 0xC2DfD02e86DDbb460A4304A0Faa053BEE3Ee2ad3
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x31d2bf667cb6c897a82c319056be92a7538ed4cb";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
